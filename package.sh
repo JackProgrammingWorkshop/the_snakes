8d248ceeb1e96a3e37ebdff14e067d3b4f78f90a
qiujiangkun <qiujiangkun@foxmail.com>
2021-10-17 13:59:27 +0800

    bugfixes & package.sh

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ cargo build --release
 rm -rf target/the_snakes
 mkdir target/the_snakes
 
-cp -r target/release/the_snakes target/release/the_snakes.exe assets bin target/the_snakes/
+cp -r target/release/the_snakes assets bin target/the_snakes/
 
 cd target && zip the_snakes.zip the_snakes
 
