3700cd322a24e2a3e279d59a9d131aeb027cd686
qiujiangkun <qiujiangkun@foxmail.com>
2021-10-17 13:59:51 +0800

    bugfixes & package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ rm -rf target/the_snakes
 mkdir target/the_snakes
 
 cp -r target/release/the_snakes assets bin target/the_snakes/
-
+rm target/the_snakes.zip
 cd target && zip the_snakes.zip the_snakes
 
 echo "package is ready at target/the_snakes.zip"
